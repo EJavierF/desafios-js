5147998f3aef6199514507e6b4b4a999ebae62d2
EJavierF <javierfalcon86@gmail.com>
2021-10-10 18:28:42 -0300

    D3 -- Entrega de desafíos sobre objetos y arrays (clase 9/10)

--- app.js
+++ app.js
@@ -1,21 +1,83 @@
 //Empezamos ejercicio de objetos
-function articulo(titulo, tema, autor, fecha) {
-    this.titulo = titulo;
-    this.tema = tema;
-    this.autor = autor;
-    this.fecha = fecha;
-    this.fechaCarga = new Date();
+
+//lo comento porque funciona, pero voy a usar la clase que es más ordenada
+// function articulo(titulo, tema, autor, fecha) {
+//     this.titulo = titulo;
+//     this.tema = tema;
+//     this.autor = autor;
+//     this.fecha = fecha;
+//     this.fechaCarga = new Date();
+
+//     this.describir = function () {
+//         alert(
+//             `El título del artículo es: ${this.titulo} \n Clasificado como: ${this.tema} \n Creado por: ${this.autor} \n Escrito el día: ${this.fecha} \n Cargado en el sitio el: ${this.fechaCarga}`
+//         );
+//     };
+// }
+class articulo {
+    constructor(titulo, tema, autor, fecha) {
+        this.titulo = titulo;
+        this.tema = tema;
+        this.autor = autor;
+        this.fecha = fecha;
+        this.fechaCarga = new Date();
+    }
+
+    describir() {
+        alert(
+            `El título del artículo es: ${this.titulo} \n Clasificado como: ${this.tema} \n Creado por: ${this.autor} \n Escrito el día: ${this.fecha} \n Cargado en el sitio el: ${this.fechaCarga}`
+        );
+    }
 }
 
-let ingTitulo = prompt("Ingresa el titulo");
-let ingTema = prompt("Tema del cual trata el artículo:");
-let ingAutor = prompt("Autor del artículo:");
-let ingFecha = prompt("Fecha:");
+// funcion para solicitar al usuario que ingrese los datos de un objeto nuevo
+const crearArticulo = () => {
+    let ingTitulo = prompt("Ingresa el titulo");
+    let ingTema = prompt("Tema del cual trata el artículo:");
+    let ingAutor = prompt("Autor del artículo:");
+    let ingFecha = prompt("Fecha en formato AAAA/MM/DD:");
+    let artNuevo = new articulo(ingTitulo.toLowerCase(), ingTema.toLowerCase(), ingAutor.toLowerCase(), new Date(ingFecha));
 
-const art01 = new articulo(ingTitulo, ingTema, ingAutor, ingFecha);
+    return artNuevo;
+};
 
-console.log(art01.titulo);
-console.log(art01.tema);
-console.log(art01.autor);
-console.log(art01.fecha);
-console.log(art01.fechaCarga);
+//vector con lista completa de articulos
+let articulosLista = [];
+
+//funcion para listar los nombres de articulos
+const mostrarArticulos = () => {
+    console.log("Lista de articulos en crudo");
+    for (const art of articulosLista) {
+        console.log(art.titulo);
+    }
+};
+//Ordenando por fecha
+const ordenaFecha = () => {
+    const artOrdFecha = articulosLista.slice().sort((a, b) => b.fecha - a.fecha);
+    console.log("Lista de articulos ordenada por fecha decreciente");
+
+    for (const art of artOrdFecha) {
+        console.log(art.fecha, " - ", art.titulo);
+    }
+};
+
+// Por practicidad para el ejercicio, sólo pido un articulo y los demás estan cargados
+const art01 = crearArticulo();
+articulosLista.push(art01);
+
+const art02 = new articulo("primera animacion stop motion", "stop motion", "papá", new Date("2021-09-24"));
+const art03 = new articulo("dibujando a Hank 3D", "dibujo", "niño", new Date("2021/08/12"));
+const art04 = new articulo("animaciones con Piskel", "animaciones", "niño", new Date("2021/05/20"));
+const art05 = new articulo("animaciones de Paint 3D", "animaciones", "papá", new Date("2021/06/19"));
+articulosLista.push(art02, art03, art04, art05);
+
+//Muestro todo el array de objetos
+console.log(articulosLista);
+
+//Imprime solo los titulos
+mostrarArticulos();
+
+//Imprime los articulos ordenados por fecha
+ordenaFecha();
+
+//art01.describir();
